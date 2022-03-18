ca2ed088b99be1da011c85a4f6a7d9328a098bc8
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-18 15:24:13 -0400

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let answer = string;
+    if (n == 0) {
+        answer = "";
+    } else if (n < 0){
+        answer = "ERROR";
+    } else {
+        for (let i = 1; i < n; i++){
+            answer = answer + string;
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
