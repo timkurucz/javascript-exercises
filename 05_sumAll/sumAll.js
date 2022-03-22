44ac1f51bc9a7163821cc11edb0fb986d82322b6
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-22 15:42:34 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let lowEnd;
+    let highEnd;
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number"){
+        return "ERROR";
+    }
+    else if (num1 > num2){
+        lowEnd = num2;
+        highEnd = num1;
+    } else {
+        lowEnd = num1;
+        highEnd = num2;
+    }
 
+    const arr = [];
+    for (let i = lowEnd; i <= highEnd; i++){
+        arr.push(i);
+    }
+
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
