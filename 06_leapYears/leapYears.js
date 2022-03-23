6dafe1243b5a9be6dff6249e1bb55432dfea7049
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-23 15:45:36 -0400

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year != "number"){
+        return "ERROR";
+    } else if (year % 400 == 0){
+        return true;
+    } else if (year % 100 == 0){
+        return false;
+    } else if (year % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
