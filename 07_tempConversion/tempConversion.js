9e9c83c7c537bcd5e333d3cd41ccbe4291ae556f
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-25 11:34:44 -0400

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let answer = (temp - 32) * (5 / 9);
+    answer = Math.round(answer * 10) / 10;
+    return answer;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let answer = (temp * (9/5)) + 32;
+  answer = Math.round(answer * 10) / 10;
+  return answer;
 };
 
 // Do not edit below this line
